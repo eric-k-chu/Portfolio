f0314da3cdc828d6bddae6dfc5d0810568a37faf
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-15 13:18:13 -0800

    added feature card design for mobile and desktop

--- portfolio/src/components/ProjectFeatureCard.tsx
+++ portfolio/src/components/ProjectFeatureCard.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 export function ProjectFeatureCard({ feature }: Props) {
   return (
-    <div className="flex w-full items-center py-4">
+    <div className="flex w-full flex-col items-center justify-center gap-8 py-4 odd:md:flex-row-reverse even:md:flex-row">
       <motion.div
-        className="h-96 w-1/2"
+        className="h-96 w-full md:w-1/2"
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
       >
@@ -17,12 +17,14 @@ export function ProjectFeatureCard({ feature }: Props) {
         <motion.div className="h-full w-full rounded-md bg-black shadow-sm" />
       </motion.div>
       <motion.div
-        className="flex w-1/2 flex-col gap-y-8 pl-4"
+        className="flex w-full flex-col gap-y-4 p-2 md:w-1/2 md:gap-y-8"
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
       >
-        <h2 className="font-semibold italic">{feature.feat}</h2>
-        <h3 className="font-light">{feature.desc}</h3>
+        <h2 className="text-base font-semibold italic md:text-xl">
+          {feature.feat}
+        </h2>
+        <h3 className="text-sm font-light md:text-lg">{feature.desc}</h3>
       </motion.div>
     </div>
   );
